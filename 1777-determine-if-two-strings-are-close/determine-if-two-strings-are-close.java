45c3950337a815d7f350222a3f3aaf45c8bef7b6
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2024-04-01 20:22:36 +0530

    Time: 45 ms (41.19%) | Memory: 45.8 MB (13.98%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -3,31 +3,25 @@ class Solution {
         // store word1 into map1 with its freq
         // store word2 into map2 with its freq
         if (word1.length() != word2.length())return false;
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-        for (int i=0; i<word1.length(); i++) {
-            map1.put(word1.charAt(i), map1.getOrDefault(word1.charAt(i), 0) + 1);
-            map2.put(word2.charAt(i), map2.getOrDefault(word2.charAt(i), 0) + 1);
-        }
-        // checks if both atleast have same letters 
-        for (int i=0; i<word2.length(); i++) {
-            if (!map1.containsKey(word2.charAt(i)) || !map2.containsKey(word1.charAt(i)))
-            return false;
-        }
-        
-        Map<Integer, Integer> map3 = new HashMap<>();
-        for (Character c : map1.keySet()) {
-            int map1Freq = map1.get(c);
-            map3.put(map1Freq, map3.getOrDefault(map1Freq, 0) + 1);
-        }
-        for (Character c : map2.keySet()) {
-            int map2Freq = map2.get(c);
-            map3.put(map2Freq, map3.getOrDefault(map2Freq, 0) - 1);
-        }
-        for (Integer i : map3.keySet()) {
-            if (map3.get(i) != 0)return false;
-        }
 
+        int[] map1 = new int[27];
+        int[] map2 = new int[27];
+        Set<Character> set = new HashSet<>();
+        for (int i=0; i<word1.length(); i++) {
+            map1[word1.charAt(i) - 'a']++;
+            map2[word2.charAt(i) - 'a']++;
+            set.add(word1.charAt(i));
+        }
+        for (int i=0; i<word2.length(); i++) {
+            if (set.contains(word2.charAt(i)))set.remove(word2.charAt(i));
+        }
+        if (!set.isEmpty())return false;
+        // checks if both atleast have same letters 
+        Arrays.sort(map1);
+        Arrays.sort(map2);
+        for (int i=0; i<map1.length; i++) {
+            if (map1[i] != map2[i])return false;
+        }
 
         return true;
     }
