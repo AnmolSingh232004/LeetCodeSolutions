4993f64f9e4d30c2791b7b8bceea6915e40c8f4c
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2024-04-01 14:10:58 +0530

    Time: 12 ms (45.29%) | Memory: 45.3 MB (70.60%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -10,19 +10,19 @@ class Solution {
         }
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        Set<Integer> set2T = new HashSet<>();
+        Set<Integer> setT = new HashSet<>();
         for (int i=0; i<nums1.length; i++) {
-            if (set2.contains(nums1[i]) || set2T.contains(nums1[i]))continue;
+            if (set2.contains(nums1[i]) || setT.contains(nums1[i]))continue;
             temp.add(nums1[i]);
-            set2T.add(nums1[i]);
+            setT.add(nums1[i]);
         }
         res.add(temp);
         temp = new ArrayList<>();
-        Set<Integer> set1T = new HashSet<>();
+        setT = new HashSet<>();
         for (int i=0; i<nums2.length; i++) {
-            if (set1.contains(nums2[i]) || set1T.contains(nums2[i]))continue;
+            if (set1.contains(nums2[i]) || setT.contains(nums2[i]))continue;
             temp.add(nums2[i]);
-            set1T.add(nums2[i]);
+            setT.add(nums2[i]);
         }
         res.add(temp);
         temp = new ArrayList<>();
