5323f9e45aab9762402a0ff987883ab4abeca210
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2024-04-01 19:54:05 +0530

    Time: 90 ms (10.36%) | Memory: 45.4 MB (62.13%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
+        // store word1 into map1 with its freq
+        // store word2 into map2 with its freq
         if (word1.length() != word2.length())return false;
         Map<Character, Integer> map1 = new HashMap<>();
         Map<Character, Integer> map2 = new HashMap<>();
-        int max1 = 0;
-        int max2 = 0;
         for (int i=0; i<word1.length(); i++) {
             map1.put(word1.charAt(i), map1.getOrDefault(word1.charAt(i), 0) + 1);
             map2.put(word2.charAt(i), map2.getOrDefault(word2.charAt(i), 0) + 1);
-            max1 = Math.max(max1, map1.get(word1.charAt(i)));
-            max2 = Math.max(max2, map2.get(word2.charAt(i)));
         }
         for (int i=0; i<word2.length(); i++) {
             if (!map1.containsKey(word2.charAt(i)) || !map2.containsKey(word1.charAt(i)))
             return false;
         }
-
-        if (max1 != max2)return false;
 
         Map<Integer, Integer> map3 = new HashMap<>();
         for (Character c : map1.keySet()) {
