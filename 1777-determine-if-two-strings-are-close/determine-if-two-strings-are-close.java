82e77425b81449f635b91c287ea482eb0eb4921b
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2024-04-01 19:42:17 +0530

    Time: 119 ms (5.17%) | Memory: 45.7 MB (21.57%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -5,22 +5,18 @@ class Solution {
         Map<Character, Integer> map2 = new HashMap<>();
         int max1 = 0;
         int max2 = 0;
-        int min1 = 0;
-        int min2 = 0;
         for (int i=0; i<word1.length(); i++) {
             map1.put(word1.charAt(i), map1.getOrDefault(word1.charAt(i), 0) + 1);
             map2.put(word2.charAt(i), map2.getOrDefault(word2.charAt(i), 0) + 1);
             max1 = Math.max(max1, map1.get(word1.charAt(i)));
             max2 = Math.max(max2, map2.get(word2.charAt(i)));
-            min1 = Math.max(min1, map1.get(word1.charAt(i)));
-            min2 = Math.max(min2, map2.get(word2.charAt(i)));
         }
         for (int i=0; i<word2.length(); i++) {
             if (!map1.containsKey(word2.charAt(i)) || !map2.containsKey(word1.charAt(i)))
             return false;
         }
 
-        if (max1 != max2 || min1 != min2)return false;
+        if (max1 != max2)return false;
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
         for (Character c : map1.keySet()) {
