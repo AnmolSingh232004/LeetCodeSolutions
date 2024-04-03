690c4439312bda05d0cbb08bc05dd3b031144279
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2024-04-03 12:06:21 +0530

    Time: 17 ms (20.49%) | Memory: 44.3 MB (50.63%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())return false;
+        int len = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i=0; i<len; i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i=0; i<len; i++) {
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+            if (map.get(t.charAt(i)) < 0)return false;
+        }
+        return true;
+    }
+}
