deab64b01c11be129401c01074f0531fa17faf41
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2024-05-08 22:53:28 +0530

    Time: 16 ms (90.98%) | Memory: 53.6 MB (64.71%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -6,22 +6,12 @@ class Solution {
         int min = 0;
 
         while (p1 <= p2) {
-        if (people[p1] + people[p2] == limit) {
-            p2--;
+            if (people[p1] + people[p2] <= limit) { 
+                p2--;
             p1++;
             min++;
-        } 
-        // 2,2,2,6,6,7,10,10,10,11,12,13,18,22,26,33,41,47,49,50
-            else if (people[p1] + people[p2] < limit) { 
-                int localSum = people[p1] + people[p2];
-                if (localSum <= limit && p1 < people.length) {
-                    p1++;
-                    if (p1 < people.length)localSum += people[p1];
-                }
-                min++;
-                p2--;
             } 
-             else if (people[p2] <= limit && p2 >= 0) {
+            else if (people[p2] <= limit && p2 >= 0) {
                 p2--;
                 min++;
             }
