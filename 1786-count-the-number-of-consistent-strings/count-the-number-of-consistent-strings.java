c7770aca3b85ca6baf585499ae6df4d81a56ab84
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2024-09-13 13:19:08 +0530

    Time: 12 ms (63.48%) | Memory: 44.8 MB (98.92%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
@@ -8,7 +8,6 @@ class Solution {
         int valid = words.length;
 
         for (int i=0; i<words.length; i++) {
-            Set<Character> localSet = new HashSet<>();
             for (int j=0; j<words[i].length(); j++) {
                 if (!set.contains(words[i].charAt(j))) {
                 valid--;
