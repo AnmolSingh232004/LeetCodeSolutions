fc9221caab20aa8a9297cb7b06dca2f838698a1a
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2024-12-04 13:13:25 +0530

    Time: 3 ms (26.73%) | Memory: 42.8 MB (84.92%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for (int i=0; i<arr.length; i++) {
+            for (int j=i+1; j<arr.length; j++) {
+                if (arr[i] == arr[j] * 2)return true;
+                if (arr[j] == arr[i] * 2)return true;
+                if (arr[i] == 0)continue;
+            }
+        }
+        return false;
+    }
+}
