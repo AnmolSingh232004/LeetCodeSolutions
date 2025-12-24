91bf59d88843be54c6077a5e3ce960dd0f4944ea
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2025-12-24 14:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (31.37%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,23 +1,30 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
-        int i = 0;
-        while (i <= end) {
-            if (nums[i] == 0) {
-                int temp = nums[i];
-                nums[i] = nums[start];
-                nums[start] = temp;
-                start++;
-                i++;
-            } else if (nums[i] == 2) {
-                int temp = nums[i];
-                nums[i] = nums[end];
-                nums[end] = temp;
-                end--;
-            } else {
-                i++;
-            }
+        int[] count = new int[3];
+        for (int i=0; i<nums.length; i++) {
+            count[nums[i]]++;
+            //freq count {2,2,2}
+        }
+        
+        int i = count[0];
+        int j = 0;
+
+        while (j < i) { // 0 < 2 -> 1 < 2 
+            nums[j] = 0;
+            j++;
+        }
+
+        i += count[1];
+        while (j < i) { // 1 < 2
+            nums[j] = 1;
+            j++;
+        }
+
+        i += count[2];
+    
+        while (j < i) {
+            nums[j] = 2;
+            j++;
         }
     }
 }
