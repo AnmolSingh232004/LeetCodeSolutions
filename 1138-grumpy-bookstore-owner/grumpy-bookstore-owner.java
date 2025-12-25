a852da2e61d45ab3218ce21ee844394d929e0a90
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2025-12-26 01:18:07 +0530

    Time: 4 ms (44.35%) | Memory: 48.2 MB (87.99%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -12,22 +12,22 @@ class Solution {
         int winVal = 0; //Win value for the static vindow
 
         for (int i=0; i<minutes; i++) { //calculating winVal for initial window which will be of size -> minutes
-            if (i < customers.length && grumpy[i] == 1)winVal += customers[i];
+            if (grumpy[i] == 1)winVal += customers[i];
         }
         
         int winMax = 0; //largest winVal
 
-        while (lo < customers.length || hi < customers.length) {
-            if (winVal > winMax) { // if local winVal is larger than winMax
-                winMax = Math.max(winMax, winVal);
-            } 
+        while (lo < customers.length || hi < customers.length) { 
+            // if local winVal is larger than winMax
+            winMax = Math.max(winMax, winVal);
+
             // operations of removing or adding value only happens when old man is grumpy
             if (lo < customers.length && grumpy[lo] == 1) { // reduce window from left and remove value
-                winVal = winVal - customers[lo];
+                winVal -= customers[lo];
             }
             lo++; //shrink the window
             if (hi < customers.length && grumpy[hi] == 1) { //add value from right if grandpa is grumpy for that value
-                winVal = winVal + customers[hi];
+                winVal +=customers[hi];
             }
             hi++; //expand the window
             
