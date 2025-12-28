569faf1f37d0c7640ded939e6a4e98c6bcd72356
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2025-12-28 23:08:08 +0530

    Time: 31 ms (64.97%) | Memory: 94.4 MB (64.87%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -3,17 +3,17 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         int res = 0;
 
-        for (int i=0; i<nums.length; i++) {
+        for (int i=0; i<nums.length; i++) { // fill hashset with nums so no duplicates
             set.add(nums[i]);
         }
 
-        for (int num : set) {
+        for (int num : set) { // num = element in set
             int localRes = 0;
             int curr = 0;
 
-            if (!set.contains(num - 1)) { // root element of any subsequence
+            if (!set.contains(num - 1)) { // if root element of any subsequence is found we process
 
-            while (set.contains(num + curr)) {
+            while (set.contains(num + curr)) { // if set contains num + curr (curr = 1+1+1+1) || basically all sucessor of num
             localRes++;
             curr++;
             }
