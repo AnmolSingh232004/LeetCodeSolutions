7b3e6e1fc08ac0d3ab2ff8d5b645687ca56b70be
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2025-12-29 01:35:38 +0530

    Time: 2 ms (96.24%) | Memory: 48.5 MB (20.99%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,33 +1,17 @@
 class Solution {
-    public int bs(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        int mid = 0;
-
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            if (nums[mid] < target) {
-                start = mid + 1;
-            } else if (nums[mid] > target) {
-                end = mid - 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-    }
-
     public int[] twoSum(int[] numbers, int target) {
-        for (int i=0; i<numbers.length; i++) {
-            int localTar = target - numbers[i];
-            int ind = bs(numbers, localTar);
-            if (ind != -1 && ind != i) {
-                int small = Math.min(i, ind);
-                int large = Math.max(i, ind);
-                if (numbers[i] + numbers[ind] == target)return new int[]{small+1, large+1};
+        int lo = 0;
+        int hi = numbers.length-1;
+
+        while (lo < hi) {
+            if (numbers[lo] + numbers[hi] > target) {
+                hi--;
+            } else if (numbers[lo] + numbers[hi] < target) {
+                lo++;
+            } else if (numbers[lo] + numbers[hi] == target){
+                return new int[]{lo+1, hi+1};
             }
         }
-        return new int[]{0};
+        return new int[]{0,0};
     }
 }
-
