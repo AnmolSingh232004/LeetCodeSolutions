db7f6d86fd932ac68a5ea443cd0953f8a99b0430
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-01 02:28:36 +0530

    Time: 1 ms (99.80%) | Memory: 94.6 MB (28.14%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -7,15 +7,17 @@ class Solution {
 
         int maxProfit = 0; // records max profit
 
-        while (hi < prices.length) {
-            if (prices[low] > prices[hi]) { 
+        while (hi < prices.length) { // while right pointer is valid
+            if (prices[low] > prices[hi]) { // if prices in present in more than in future in that case ce skip move into the future
                 while (prices[low] <= prices[hi])hi++;
                 low++;
-            } else if (prices[low] <= prices[hi]) {
+            } else if (prices[low] < prices[hi]) { // if prices in present is cheaper than in future 
                 int localProfit = prices[hi] - prices[low];
                 maxProfit = Math.max(localProfit, maxProfit);
                 hi++;
-            }
+            } else {
+                hi++;
+                }
 
         }
         return maxProfit;
