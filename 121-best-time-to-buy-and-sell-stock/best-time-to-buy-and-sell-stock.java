072761e2287195e6c5158cb501459cfa4961fe69
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-01 18:18:11 +0530

    Time: 1 ms (99.80%) | Memory: 93.6 MB (95.86%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -2,15 +2,15 @@ class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
 
-        int lo = 0;
-        int hi = 1;
+        int lo = 0; // left pointer, aka past
+        int hi = 1; // right pointer, aka future
 
         while (hi < prices.length) {
-            if (prices[lo] <= prices[hi]) {
-                maxProfit = Math.max(maxProfit, prices[hi] - prices[lo]);
-                hi++;
-            } else {
-                lo = hi;
+            if (prices[lo] <= prices[hi]) { // if stock was cheap in past and expensive in future calc local profit and move hi
+                maxProfit = Math.max(maxProfit, prices[hi] - prices[lo]); // local profit vs maxProfit 
+                hi++; // move hi since its buying at hi will give loss 100%
+            } else if (prices[lo] > prices[hi]) { // if stock is expensive today than future then we move our lo to hi 
+                lo = hi; // move to lo to hi
             }
         }
         return maxProfit;
