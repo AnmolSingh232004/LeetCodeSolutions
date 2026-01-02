30242aac534a693a3e8a5173b9d705068ee0df65
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-02 18:22:44 +0530

    Time: 3 ms (66.61%) | Memory: 46.5 MB (47.94%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,22 +15,19 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (subRoot == null)return true; //if subRoot is null and tree is not it means it has to be subTree since its empty
-        // very impt since once subRoot is fully traversed we will know the answer and since we use isSubtree on both left and right
-        //it ensures that 
-        if (root == null) return false; //Assuming subRoot is not null since checked in first case which means subRoot is not null
+        if (subRoot == null && root != null)return true;
+        if (root == null && subRoot != null)return false;
 
-        if(sameTree(root, subRoot))return true; //simply checks if the given trees are same or not
+        if (isSameTree(root, subRoot))return true;
 
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-        //we compare both sides so then the first subRoot == null actually makes sense
-    }
+     }
 
-    public boolean sameTree(TreeNode root, TreeNode subRoot) { // helper which tells if  2 tree are same or not
+    public boolean isSameTree(TreeNode root, TreeNode subRoot) {
         if (root == null && subRoot == null)return true;
-        if (root != null && subRoot == null || root == null && subRoot != null)return false;
         if (root != null && subRoot != null && root.val != subRoot.val)return false;
+        if ((root != null && subRoot == null) || (root == null && subRoot != null))return false;
 
-        return (sameTree(root.left, subRoot.left) && sameTree(root.right, subRoot.right));
+        return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
     }
 }
