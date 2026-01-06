4dc2a626f32e687a85d4ecb430c224f8085332c0
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-07 01:49:05 +0530

    Time: 7 ms (67.96%) | Memory: 44 MB (64.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,28 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if (s1.length() > s2.length())return false;
-        
-        int[] need = new int[26];
-        int[] window = new int[26];
+        int[] need = new int[26]; // freq of s1
+        int[] window = new int[26]; // freq of window
 
-        for (int i=0; i<s1.length(); i++) { // populate need window this is our comparison for perfect permutation
+        for (int i=0; i<s1.length(); i++) { // populate need window using s1
             need[Math.abs(s1.charAt(i)) - 'a']++;
         }
 
-        for (int i=0; i<s1.length(); i++) { // populate inditial fixed window
+        for (int i=0; i<s1.length(); i++) { // populate initial fixed window
             window[Math.abs(s2.charAt(i) - 'a')]++;
         }
 
-        if (Arrays.equals(need, window))return true;
+        if (Arrays.equals(need, window))return true; // if arrays are equal intially 
 
         int lo = 0;
         int hi = s1.length() - 1;
 
-        hi++;
+        hi++; // make hi start from outside the window
 
         while (hi < s2.length()) {
-            int charIndexHi = Math.abs( 'a' - s2.charAt(hi) );
-            int charIndexLo = Math.abs( 'a' - s2.charAt(lo) );
+            int charIndexHi = Math.abs( 'a' - s2.charAt(hi) ); // char just after window head
+            int charIndexLo = Math.abs( 'a' - s2.charAt(lo) ); // chat at window tail
             window[charIndexHi]++;
             window[charIndexLo]--;
             hi++;
