5cc156269bb402314e32ba1ec0bca730069556a6
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-11 16:59:51 +0530

    Time: 7 ms (98.83%) | Memory: 46.5 MB (48.25%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,26 +9,16 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == q || root == p)return root;
         if (root == null)return null;
+        if (root == p || root == q)return root;
 
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if (left != null && right == null) {
-            return left;
-        } else if (right != null && left == null) {
-            return right;
-        } else if (left != null && right != null) {
-            return root;
-        } else {
-            return null;
-        }
+        if (left == null && right != null)return right;
+        if (left != null && right == null)return left;
+        if (left != null && right != null)return root;
 
+        return null;
     }
 }
-
-// we go in one direction if no p or q is found we return null from that side
-// if p or q is found on either side we return p and q from each side
-// if p or q is found on one side then we return the one we found first
-// if root recives two non nulls it is answer
