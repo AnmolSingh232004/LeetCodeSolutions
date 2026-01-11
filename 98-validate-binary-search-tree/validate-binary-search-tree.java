444cc7193a0744b0045d1436316e7c59505f3df2
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-12 00:53:06 +0530

    Time: 2 ms (13.86%) | Memory: 45.5 MB (17.29%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    boolean res = true;
+
+    public boolean checkIncreasing(List<Integer> list) {
+        int var = list.get(0);
+        for (int i=1; i<list.size(); i++) {
+            if (list.get(i) <= list.get(i-1))return false;
+        }
+
+        return true;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        inOrder(root);
+        return checkIncreasing(list);
+    }
+
+    public void inOrder(TreeNode root) {
+        if (root == null)return;
+
+        inOrder(root.left);
+        list.add(root.val);
+        inOrder(root.right);
+    }
+
+}
