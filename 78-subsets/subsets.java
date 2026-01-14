3154843d70f2c8fa4a1cbe8fc4eca9b3bd035c91
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-14 23:23:16 +0530

    Time: 1 ms (88.00%) | Memory: 44.2 MB (62.59%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,30 +1,25 @@
 class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
-        dfs(nums, 0, list, res);
+        List<Integer> subset = new ArrayList<Integer>();
+
+        dfs(nums, 0, subset);
         return res;
     }
 
-    public void dfs(int[] nums , int i, List<Integer> list, List<List<Integer>> res) {
+    public void dfs(int[] nums, int i, List<Integer> subset) {
         if (i >= nums.length) {
-            res.add( new ArrayList<>(list));
+            res.add(new ArrayList<>(subset));
             return;
         }
 
-        list.add(nums[i]);
-        dfs(nums, i+1, list, res);
+        subset.add(nums[i]);
+        dfs(nums, i+1, subset);
 
-        list.remove(list.size() - 1);
-        dfs(nums, i+1, list, res);
+        subset.remove(subset.size() - 1);
+        dfs(nums, i+1, subset);
+
 
     }
 }
-
-// we can generate all subsets if we initially start with 1st element on nums
-// and do both the options 1 has initally 
-// which is include 1 or not include 1 if it doesnt include we get [], if it does we  get [1]
-// then [] can include 2 or not we get [2], []
-// then [1] can [1,2], [1]
-// [1,2] can include 3 | [1,2,3] or [1,2]
-// like this we will get all possible subsets
