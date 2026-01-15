0580837c165fab196e5110f430ac83b7ea97fd25
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-15 23:00:30 +0530

    Time: 2 ms (95.07%) | Memory: 45.8 MB (40.24%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,25 +2,26 @@ class Solution {
     List<List<Integer>> res = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> comb = new ArrayList<>();
-        dfs(candidates, 0, target, comb);
+        List<Integer> list = new ArrayList<>();
+
+        dfs(candidates, 0, target, list);
         return res;
     }
 
-    public void dfs(int[] cand, int i, int target, List<Integer> comb) {
-        if (i >= cand.length || target < 0) { // invalid combination
+    public void dfs(int[] nums, int i, int target, List<Integer> list) {
+        if (i == nums.length || target < 0) {
             return;
         }
-        if (target == 0) { // valid combination found
-        res.add(new ArrayList<>(comb));
-        return;
+
+        if (target == 0) {
+            res.add(new ArrayList<>(list));
+            return;
         }
 
-        comb.add(cand[i]);
-        dfs(cand, i, target - cand[i], comb); // // stay at i to allow reuse of cand[i], other elements are still reachable later via skip calls.
-        comb.remove(comb.size()-1);
-        dfs(cand, i+1, target, comb); // // move to next index to permanently skip cand[i]; target unchanged because nothing was used
-
+        list.add(nums[i]);
+        dfs(nums, i, target - nums[i], list); // this just says include nums[i] again and again for one branch
+        list.remove(list.size() - 1);
+        dfs(nums, i+1, target, list); // this says for one branch ignore i forever and since we ignore i dont change target
 
     }
 }
