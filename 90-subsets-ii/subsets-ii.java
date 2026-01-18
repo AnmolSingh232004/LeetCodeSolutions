c65df454bed6bc33888d7fc30ff81951853c3442
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-18 19:50:25 +0530

    Time: 2 ms (99.23%) | Memory: 45.4 MB (16.74%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -2,31 +2,24 @@ class Solution {
     List<List<Integer>> res = new ArrayList<>();
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        dfs(nums, 0, new ArrayList<Integer>(), new HashSet<List<Integer>>());
+        Arrays.sort(nums);
 
+        dfs(nums, 0, new ArrayList<>());
         return res;
     }
 
-    public void dfs(int[] nums, int i, List<Integer> subset, Set<List<Integer>> exists) {
+    public void dfs(int[] nums, int i, List<Integer> subset) {
         if (i == nums.length) {
-            List<Integer> copy = new ArrayList<>(subset);
-            Collections.sort(copy);
-
-            if (!exists.contains(copy)) {
-            res.add(new ArrayList<>(subset));
-            exists.add(copy);
-            }
+            res.add( new ArrayList<>(subset) );
             return;
         }
 
         subset.add(nums[i]);
-        dfs(nums, i+1, subset, exists);
+        dfs(nums, i+1, subset);
         subset.remove(subset.size() - 1);
 
-        dfs(nums, i+1, subset, exists);
+        while (i < nums.length-1 && nums[i] == nums[i+1])i++;
+
+        dfs(nums, i+1, subset);
     }
 }
-
-// In this soln what we simply do is, generate all the subsets, and before adding it to our result we just check if it already exsits or not in our res, if it does we skip else we add it.
-
-// We can check this by sorting our potential subset before adding it and checking if it exists or not, this helps because this only depends on frequency of elements
