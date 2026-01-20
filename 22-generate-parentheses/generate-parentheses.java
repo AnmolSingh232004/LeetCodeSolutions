304cc38b34a73f287f2797b3f30a53b0e636d034
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-20 16:49:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (81.20%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -3,41 +3,23 @@ class Solution {
 
     public List<String> generateParenthesis(int n) {
         dfs(n, 0, 0, new StringBuilder());
-
-        return res;
+        return res;    
     }
 
-    public void dfs(int n, int openCount, int closeCount, StringBuilder paren) {
+    public void dfs(int n, int openCount, int closeCount, StringBuilder sb) {
         if (openCount == n && closeCount == n) {
-            res.add(new String(paren));
+            res.add(new String(sb));
             return;
         }
-        
-        if (openCount > n || closeCount > n || closeCount > openCount) {
-            return;
-        }
+       
+        if (closeCount > openCount || openCount > n || closeCount > n)return;
 
-        paren.append('(');
-        dfs(n, openCount + 1, closeCount, paren);
-        paren.deleteCharAt(paren.length() - 1);
+        sb.append('(');
+        dfs(n, openCount + 1, closeCount, sb);
+        sb.deleteCharAt(sb.length() - 1);
 
-        paren.append(')');
-        dfs(n, openCount, closeCount + 1, paren);
-        paren.deleteCharAt(paren.length() - 1);
-
+        sb.append(')');
+        dfs(n, openCount, closeCount + 1, sb);
+        sb.deleteCharAt(sb.length() - 1);
     }
 }
-
-// every call will have option to include ( or )
-// we will keep track of open and close parenthese
-// if at any point either surpasses 'n' which means more than half the elements have ( or ), in that case we return since its bad case 
-// and also techincally ) should never outnumber open therefore if closeCount > openCount that is bad parentheses too
-
-// if opencount == n && clsoecount == n, this means both add upto a valid parentheses since these will have ( always before ) thanks to basecases
-// in this case we add this combo to res
-
-// we add (, increase openCount, then backtrack with it and remove it later
-
-// then we add ), backtrack with it and remove later
-
-// we must ensure we increase open count when adding ( and increase clsoe count when adding )
