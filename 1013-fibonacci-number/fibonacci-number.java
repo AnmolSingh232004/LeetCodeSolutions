ea4f5062c831e5046448d7963d978f5127a19145
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-26 01:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (23.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,20 @@
 class Solution {
+    int[] cache;
     public int fib(int n) {
+        cache = new int[n+1];
+        for (int i=0; i<=n; i++) {
+            cache[i] = -1;
+        }
+        return dfs(n);
+    }
+
+    public int dfs(int n) {
         if (n == 0)return 0;
         if (n == 1)return 1;
+        if (cache[n] != -1)return cache[n]; 
 
-        return fib(n-1) + fib(n-2);
+        cache[n] = dfs(n-1) + dfs(n-2);
+
+        return dfs(n-1) + dfs(n-2);
     }
 }
-
-// f 0 = 0
-// f 1 = 1
-// f 2 = f1 + f0 = 1
-// f 3 = f2 + 1 = 2
-// f 4 = f2 + f3 = 1+2 = 3
