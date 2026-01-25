cabfaa7ca428df54da796563e40db5ecd8ebb4fc
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-26 02:26:28 +0530

    Time: 1 ms (38.28%) | Memory: 45.1 MB (32.61%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -6,23 +6,21 @@ class Solution {
         for (int i=0; i<=cost.length; i++) {
             cache[i] = -1;
         }
-        int res1 = dfs(cost, 0, 0);
-        int res2 = dfs(cost, 1, 0);
+        int res1 = dfs(cost, 0);
+        int res2 = dfs(cost, 1);
 
         return Math.min(res1, res2);
     }
 
-    public int dfs(int[] cost, int i, int currCost) {
+    public int dfs(int[] cost, int i) {
         if (i >= cost.length)return 0;
         if (cache[i] != -1)return cache[i];
-
-        currCost = cost[i];
         
+        int one = dfs(cost, i+1);
+        int two = dfs(cost, i+2);
 
-        int one = dfs(cost, i+1, currCost);
-        int two = dfs(cost, i+2, currCost);
-        cache[i] = currCost + Math.min(one, two);
+        cache[i] = cost[i] + Math.min(one, two);
 
-        return currCost + Math.min(one, two);
+        return cost[i] + Math.min(one, two);
     }
 }
