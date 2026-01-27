3264b7a6d02f8278164deea681bc03d3a54ad51a
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-27 17:01:24 +0530

    Time: 22 ms (36.30%) | Memory: 46.9 MB (32.68%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,37 +1,34 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int lo = 0;
-        int hi =0;
+        if (s.length() == 1)return s;
         String res = "";
-        Map<int[], String> map = new HashMap<>();
+        
+        for (int i=0; i<s.length(); i++) {
+            int lo = i;
+            int hi = i;
 
-        while (lo < s.length() && hi < s.length()) {
-            if ( isPalin(s, lo, hi) ) {
-                if (map.containsKey(new int[]{lo, hi}) && res.length() < map.get(new int[]{lo, hi}).length() )res = s.substring(lo, hi+1);
-
-                else if (s.substring(lo, hi+1).length() > res.length()) {
-                res = s.substring(lo, hi+1);
-                map.put(new int[]{lo, hi}, res);
+            while (lo >= 0 && hi < s.length() && s.charAt(lo) == s.charAt(hi)) {
+                if (res.length() < hi - lo + 1) {
+                    res = s.substring(lo, hi+1);
                 }
-
+                lo--;
+                hi++;
             }
-            if (hi == s.length() - 1) {
-                lo++;
-                hi = lo;
-            }
-            hi++;
 
-            
+            lo = i;
+            hi = i+1;
+            while (lo >= 0 && hi < s.length() && s.charAt(lo) == s.charAt(hi)) {
+                if (res.length() < hi - lo + 1) {
+                    res = s.substring(lo, hi+1);
+                }
+                lo--;
+                hi++;
+            }
+
         }
         return res;
     }
-
-    public boolean isPalin(String s, int lo, int hi) {
-        while (lo <= hi) {
-            if (s.charAt(lo) != s.charAt(hi))return false;
-            hi--;
-            lo++;
-        }
-        return true;
-    }
 }
+
+// How about we start comparing from the middle
+// so for babad, we do start our pointers from [1] which is a and exapnd on both side until valid and then adanvce our pointers
