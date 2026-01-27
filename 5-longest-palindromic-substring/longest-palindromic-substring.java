053369a92fa58ec37ce8a91c3bb97ab49d397780
Anmol Singh <141349539+AnmolSingh232004@users.noreply.github.com>
2026-01-27 17:11:52 +0530

    Time: 126 ms (26.21%) | Memory: 47.2 MB (15.80%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,34 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() == 1)return s;
         String res = "";
-        
         for (int i=0; i<s.length(); i++) {
             int lo = i;
             int hi = i;
 
+            // if center is for odd palindromes
             while (lo >= 0 && hi < s.length() && s.charAt(lo) == s.charAt(hi)) {
-                if (res.length() < hi - lo + 1) {
-                    res = s.substring(lo, hi+1);
-                }
+                if (res.length() < s.substring(lo, hi+1).length() )res = s.substring(lo, hi+1);
                 lo--;
                 hi++;
             }
-
             lo = i;
             hi = i+1;
+
+            // if center is even
             while (lo >= 0 && hi < s.length() && s.charAt(lo) == s.charAt(hi)) {
-                if (res.length() < hi - lo + 1) {
-                    res = s.substring(lo, hi+1);
-                }
+                if (res.length() < s.substring(lo, hi+1).length() )res = s.substring(lo, hi+1);
                 lo--;
                 hi++;
             }
-
         }
         return res;
     }
 }
 
-// How about we start comparing from the middle
-// so for babad, we do start our pointers from [1] which is a and exapnd on both side until valid and then adanvce our pointers
+// How about we consider every single index a possible palindrome, and we expand from center to left and right from it to build the palindrome, how this helps is, if we did smth like, s = "babad", if we did start from [0] and expanded and checked, we would first of all have to check all substring which is already n^2 which is decent but then for checking if its a palindrome or not it would take use  O(n) as well which gives us total tc of O(n^3)
+// To fix this how about, for every index we expand from center to outside how this will help is simulatenously when checking for all substrings, we can also check if it is palindrome at the same time since exapnsion happens on edges.
+
+// We will have two types of center, one when center is odd and one where it is even, odd is simple both pointers start at same place and expand, if center is even which means it is a palindrome but smth like this "bb", normal code would think this is wrong since expanding on left with lo would go out of bound and "abb" would also be wrong since we expand from b, bb wont be considered so we can have two cases in other case we can make lo = i, hi = i+1, because since we will be doing a for loop from left to right hi+1 makes sense 
